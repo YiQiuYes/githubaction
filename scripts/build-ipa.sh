e11eb87d5a39f02831e7758ea748ab11f2b25f60
易秋 <binzixiang@163.com>
2024-01-11 18:31:19 +0800

    change workflows

--- scripts/build-ipa.sh
+++ scripts/build-ipa.sh
@@ -2,10 +2,6 @@
 
 cd "$( cd "$( dirname "$0"  )" && pwd  )/.."
 
-cd go/mobile
-gomobile init
-gomobile bind -iosversion 11.0 -target=ios -o lib/Mobile.xcframework ./
-cd ../..
 flutter pub get
 flutter build ios --release --no-codesign
 
