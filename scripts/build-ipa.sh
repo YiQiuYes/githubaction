a97733b11b6ff3ff2073530bc11be19094796b8d
易秋 <binzixiang@163.com>
2024-01-11 17:46:19 +0800

    change workflows

---
+++ scripts/build-ipa.sh
@@ -0,0 +1,17 @@
+# 构建未签名的IPA
+
+cd "$( cd "$( dirname "$0"  )" && pwd  )/.."
+
+cd go/mobile
+gomobile init
+gomobile bind -iosversion 11.0 -target=ios -o lib/Mobile.xcframework ./
+cd ../..
+flutter pub get
+flutter build ios --release --no-codesign
+
+cd build
+mkdir -p Payload
+mv ios/iphoneos/Runner.app Payload
+
+sh ../scripts/thin-payload.sh
+zip -9 nosign.ipa -r Payload
